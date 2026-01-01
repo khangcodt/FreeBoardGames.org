e852b779e95300c899dd89ce1ef522e04af5a059
Jason <me@jasonh.dev>
2025-12-31 19:28:59 -0700

    wip

--- web/server/bgio.ts
+++ web/server/bgio.ts
@@ -5,7 +5,7 @@
 import { GAMES_LIST } from 'games';
 import noCache from 'koa-no-cache';
 const cors = require('@koa/cors'); // tslint:disable-line
-import redis from 'redis';
+import { createClient } from 'redis';
 import { RedisPubSub } from '@boardgame.io/redis-pubsub';
 import { Server, SocketIO } from 'boardgame.io/server';
 import { PostgresStore } from 'bgio-postgres';
@@ -20,21 +20,23 @@ function getDb() {
   return new PostgresStore(pgUrl);
 }
 
-function getTransport() {
+async function getTransport() {
   const host = process.env.FBG_REDIS_HOST;
   const port = process.env.FBG_REDIS_PORT;
   const password = process.env.FBG_REDIS_PASSWORD;
   if (!host || !port || !password) {
     return;
   }
-  const pub = redis.createClient({
+  const pub = createClient({
     socket: { host, port: parseInt(port, 10) },
     password,
   });
-  const sub = redis.createClient({
+  const sub = createClient({
     socket: { host, port: parseInt(port, 10) },
     password,
   });
+  await pub.connect();
+  await sub.connect();
   return new SocketIO({ pubSub: new RedisPubSub(pub, sub) });
 }
 
@@ -43,7 +45,8 @@ const startServer = async () => {
   const games = (await configs).map((config) => config.default.bgioGame);
   const db = getDb();
   const origins = process.env.BGIO_PUBLIC_SERVERS || 'http://localhost';
-  const server = Server({ games, db, origins, transport: getTransport() });
+  const transport = await getTransport();
+  const server = Server({ games, db, origins, transport });
   server.app.use(noCache({ global: true }));
   server.app.use(cors());
   
