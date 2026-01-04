a1a2e2f9f879686b916b7e68c5828037427ce8cc
Jason <me@jasonh.dev>
2026-01-04 15:33:53 -0700

    wip

--- web/src/infra/i18n/hooks/useCurrentGameTranslation.ts
+++ web/src/infra/i18n/hooks/useCurrentGameTranslation.ts
@@ -8,6 +8,8 @@ export const useCurrentGameTranslation = () => {
   // Ensure we always have a valid namespace string - never null/undefined
   const namespace = game?.code ? getGameCodeNamespace(game.code) : 'common';
   
+  console.log("namespace:", namespace);
+
   // Use the ready flag to ensure translations are loaded
   const { t, ready } = useTranslation(namespace, { 
     useSuspense: false 
